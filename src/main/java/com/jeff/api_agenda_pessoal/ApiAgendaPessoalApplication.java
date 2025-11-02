12ba5c34e0a6701b261ff62a989e86930606f0c0
Jeffrey <jeff.dsv@gmail.com>
2025-11-01 21:03:55 -0400

    chore: estrutura inicial do projeto Spring Boot

---
+++ src/main/java/com/jeff/api_agenda_pessoal/ApiAgendaPessoalApplication.java
@@ -0,0 +1,13 @@
+package com.jeff.api_agenda_pessoal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAgendaPessoalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAgendaPessoalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jeff/api_agenda_pessoal/ApiAgendaPessoalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeff.api_agenda_pessoal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAgendaPessoalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
